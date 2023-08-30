c9213e7dfeb5f6d1417505281b6b63b315321bbf
abhishek2393 <coopisthebestkick@gmail.com>
2023-08-30 17:38:12 +0530

    new screen postion added

--- hellobarwidget/controller.js
+++ hellobarwidget/controller.js
@@ -6,6 +6,60 @@ class WidgetController {
       const data = await response.json();
       return data;
     }
+    
+    createWidgetContent() {
+      let widgetContent = `
+          <header class="widget__header">
+              <h3>Start a conversation</h3>
+              <p>We usually respond within a few hours</p>
+          </header>
+  
+          <form>
+              <div class="form__field">
+                  <label for="name">Name</label>
+                  <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Enter your name"
+                  />
+              </div>
+  
+              <div class="form__field">
+                  <label for="email">Email</label>
+                  <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  />
+              </div>
+  
+              <div class="form__field">
+                  <label for="subject">Subject</label>
+                  <input
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  placeholder="Enter Message Subject"
+                  />
+              </div>
+  
+              <div class="form__field">
+                  <label for="message">Message</label>
+                  <textarea
+                  id="message"
+                  name="message"
+                  placeholder="Enter your message"
+                  rows="6"
+                  ></textarea>
+              </div>
+  
+              <button>Send Message</button>
+          </form>
+      `;
+      return widgetContent;
+    }
   }
   
   export default WidgetController;
--- hellobarwidget/widget.js
+++ hellobarwidget/widget.js
@@ -1,92 +1,239 @@
-import {styles } from "./assets.js";
+import { CLOSE_ICON, MESSAGE_ICON, styles } from "./assets.js";
 // import a class
-import WidgetController from "./controller.js" ;
-class Widget{
-    constructor(){
-        this.initialize();
-        this.injectStyles();
-        
-        
-    }
+import WidgetController from "./controller.js";
+class Widget {
+  constructor() {
+    this.initialize();
+    this.injectStyles();
+    this.data=[];
+    this.open = false;
+  
 
-    async initialize() {
-        /**
-         * Create and append a div element to the document body
-         */
-        const container = document.createElement("div");
-        
-        var position = document.querySelector('script[data-id="Pxwqwer"][data-position]')?.getAttribute('data-position');
-        console.log(position);
-        if(position){
-        if(position == "top"){
-        container.style.position = "fixed";
+  }
+  
+   
+  async initialize() {
+    /**
+     * Create and append a div element to the document body
+     */
+    const container = document.createElement("div");
+    const buttonContainer = document.createElement("button");
+    const widgetIconElement = document.createElement("span");
+    const closeIconElement = document.createElement("span");
+    this.widgetContainer = document.createElement("div");
+    var position = document.querySelector('script[data-id="Pxwqwer"][data-position]')?.getAttribute('data-position');
+    var themeColor = document.querySelector('script[data-id="Pxwqwer"][data-theme]')?.getAttribute('data-theme');
+    var color = document.querySelector('script[data-id="Pxwqwer"][data-color]')?.getAttribute('data-color');
+    if (position) {
+      if (position == "top") {
+        container.style.position = "absolute";
         container.style.display = "flex";
         container.style.justifyContent = "center";
         container.style.top = "0";
         container.style.left = "0";
         container.style.right = "0";
         container.style.alignItems = "center";
-        }
-        else if(position == "bottom"){
-        container.style.position = "fixed";
+        container.style.padding = "0.25rem";
+        this.quotes = new WidgetController();
+        this.data = await this.quotes.fetchQuotes();
+        console.log("top", this.data);
+        document.body.appendChild(container);
+        this.widgetContainer.classList.add("container");
+       
+      }
+      else if (position == "bottom") {
+        container.style.position = "absolute";
+        container.style.display = "flex";
+        container.style.justifyContent = "center";
         container.style.bottom = "0";
         container.style.left = "0";
         container.style.right = "0";
-        container.style.display = "flex";
-        container.style.justifyContent = "center";
         container.style.alignItems = "center";
-        }
-        }
-        else{
-            container.style.position = "fixed";
+        container.style.padding = "0.25rem";
+        this.quotes = new WidgetController();
+        this.data = await this.quotes.fetchQuotes();
+        console.log("bottom", this.data);
+        document.body.appendChild(container);
+        this.widgetContainer.classList.add("container");
+        
+        
+      }
+      else if (position == "bottom-right") {
+        container.style.position = "fixed";
+        container.style.bottom = "30px";
+        container.style.right = "30px";
+        
+        buttonContainer.classList.add("button__container");
+
+        
+        widgetIconElement.innerHTML = MESSAGE_ICON;
+        widgetIconElement.classList.add("widget__icon");
+        this.widgetIcon = widgetIconElement;
+
+        
+        closeIconElement.innerHTML = CLOSE_ICON;
+        closeIconElement.classList.add("widget__icon", "widget__hidden");
+        this.closeIcon = closeIconElement;
+        this.data = [];
+        console.log("bottom-right", this.data);
+        buttonContainer.appendChild(this.widgetIcon);
+        buttonContainer.appendChild(this.closeIcon);
+        buttonContainer.addEventListener("click", this.toggleOpen.bind(this));
+        this.widgetContainer.classList.add("widget__hidden", "widget__container");
+        this.content = new WidgetController();
+        this.widgetContainer.innerHTML = this.content.createWidgetContent();
+
+        container.appendChild(buttonContainer);
+        document.body.appendChild(container);
+        // this.widgetContainer.classList.add("container");
+      }
+      else if (position == "mid-left") {
+        // add widget to the left side of the screen in the middle add css accordingly
+        container.style.position = "fixed";
         container.style.display = "flex";
         container.style.justifyContent = "center";
         container.style.top = "0";
         container.style.left = "0";
-        container.style.right = "0";
+        container.style.bottom = "0";
         container.style.alignItems = "center";
-        }
-    
-        // Object.keys(this.position).forEach(
-        //   (key) => (container.style[key] = this.position[key])
-        // );
+        container.style.padding = "1px";
+         var text ="Feedback"; // text for the div
+        this.data=[];
+        buttonContainer.classList.add("container-mid");
+        buttonContainer.innerHTML = text;
+        buttonContainer.style.backgroundColor = themeColor;
+        buttonContainer.style.color = color;
+        
+        buttonContainer.style.writingMode = "vertical-rl";
+        buttonContainer.style.textOrientation = "upright";
+        
+        closeIconElement.innerHTML = CLOSE_ICON;
+        closeIconElement.classList.add("widget__icon", "widget__hidden");
+        this.closeIcon = closeIconElement;
+
+        buttonContainer.appendChild(this.closeIcon);
+        buttonContainer.addEventListener("click", this.toggleFeedBack.bind(this));
+        this.widgetContainer.classList.add("widget__hidden", "widget__mid__container__left");
+        this.content = new WidgetController();
+        this.widgetContainer.innerHTML = this.content.createWidgetContent();
+        
+        
         document.body.appendChild(container);
-    
-        /**
-         * Create a button element and give it a class of button__container
-         */
-        const widgetContainer = document.createElement("div");
-        widgetContainer.classList.add("container");
-        // fetch quote and append that to widget container
+        container.appendChild(buttonContainer);
         
-        const quotes = new WidgetController();
-        const data = await quotes.fetchQuotes();
-        const randomQuote = data[Math.floor(Math.random() * data.length)];
-        const quote = document.createElement("p");
-        quote.innerHTML = randomQuote.text;
-        quote.style.color = "#fff";
-        quote.style.fontSize = "1.5rem";
-        quote.style.fontWeight = "bold";
-        quote.style.textAlign = "center";
-        quote.style.marginTop = "1rem";
-        widgetContainer.appendChild(quote);
-        
-        
-        
-    
-       
-    
-        container.appendChild(widgetContainer);
+
       }
-    
-    
-    
-      injectStyles() {
-        const styleTag = document.createElement("style");
-        styleTag.innerHTML = styles.replace(/^\s+|\n/gm, "");
-    
-        document.head.appendChild(styleTag);
+      else if (position == "mid-right") {
+      // add widget to the right side of the screen in the middle add css accordingly
+      container.style.position = "fixed";
+      container.style.display = "flex";
+      container.style.justifyContent = "center";
+      container.style.top = "0";
+      container.style.right = "0";
+      container.style.bottom = "0";
+      container.style.alignItems = "center";
+      container.style.padding = "1px";
+      var text ="Feedback"; // text for the div
+      this.data=[];
+      buttonContainer.classList.add("container-mid");
+      buttonContainer.innerHTML = text;
+      buttonContainer.style.backgroundColor = themeColor;
+      buttonContainer.style.color = color;
+
+      buttonContainer.style.writingMode = "vertical-rl";
+      buttonContainer.style.textOrientation = "upright";
+
+      closeIconElement.innerHTML = CLOSE_ICON;
+        closeIconElement.classList.add("widget__icon", "widget__hidden");
+        this.closeIcon = closeIconElement;
+
+        buttonContainer.appendChild(this.closeIcon);
+        buttonContainer.addEventListener("click", this.toggleFeedBack.bind(this));
+        this.widgetContainer.classList.add("widget__hidden", "widget__mid__container__right");
+        this.content = new WidgetController();
+        this.widgetContainer.innerHTML = this.content.createWidgetContent();
+
+      document.body.appendChild(container);
+      container.appendChild(buttonContainer);
+        
+
       }
+    }
+    else {
+      console.log("no position");
+      this.data = [];
+      container.style.position = "relative";
+      container.style.display = "flex";
+      container.style.justifyContent = "center";
+      container.style.top = "0";
+      container.style.left = "0";
+      container.style.right = "0";
+      container.style.alignItems = "center";
+    }
+   
+
+   
+    
+    // fetch quote and append that to widget container
+    
+    if(this.data.length > 0 ){
+    const randomQuote = this.data[Math.floor(Math.random() * this.data.length)];
+    const quote = document.createElement("p");
+    quote.innerHTML = randomQuote.text;
+    quote.style.color = color;
+    quote.style.fontSize = "1.5rem";
+    quote.style.fontWeight = "bold";
+    quote.style.textAlign = "center";
+    quote.style.marginTop = "1rem";
+    this.widgetContainer.style.backgroundColor = themeColor;
+    this.widgetContainer.appendChild(quote);
+
+    }
+
+
+
+
+    container.appendChild(this.widgetContainer);
+  }
+
+
+
+  injectStyles() {
+    const styleTag = document.createElement("style");
+    styleTag.innerHTML = styles.replace(/^\s+|\n/gm, "");
+
+    document.head.appendChild(styleTag);
+  }
+  toggleOpen() {
+    this.open = !this.open;
+    if (this.open) {
+      this.widgetIcon.classList.add("widget__hidden");
+      this.closeIcon.classList.remove("widget__hidden");
+      this.widgetContainer.classList.remove("widget__hidden");
+    } else {
+      this.widgetContainer.innerHTML = this.content.createWidgetContent();
+      this.widgetIcon.classList.remove("widget__hidden");
+      this.closeIcon.classList.add("widget__hidden");
+      this.widgetContainer.classList.add("widget__hidden");
+    }
+  }
+
+  toggleFeedBack() {
+    
+  console.log("clicked");
+  this.open = !this.open;
+  if(this.open){
+    this.widgetContainer.classList.remove("widget__hidden");
+    this.closeIcon.classList.remove("widget__hidden");
+  }
+  else{
+    this.widgetContainer.innerHTML = this.content.createWidgetContent();
+    this.widgetContainer.classList.add("widget__hidden");
+    this.closeIcon.classList.add("widget__hidden");
+  }
+
+
+  }
 
 }
 
